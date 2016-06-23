9f5c41ed104ef67779b3d7ff2fbf6fa21d451c84
Maria Celeste Fernandez Zuñiga <maryce15_@hotmail.com>
2016-06-22 22:11:18 -0600

    Celeste Sprint 1

---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraChange : MonoBehaviour {
+    public Camera thirdPersonCamera;
+    public Camera firstPersonCamera;
+	// Use this for initialization
+	void Start () {
+        firstPersonCamera.enabled = true;
+        thirdPersonCamera.enabled=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("f1"))
+        {
+            firstPersonCamera.enabled = false;
+            thirdPersonCamera.enabled = true;
+        }
+
+        if (Input.GetButtonDown("f2"))
+        {
+            firstPersonCamera.enabled =true;
+            thirdPersonCamera.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/MoveScripts/MoveObjects.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class MoveObjects : MonoBehaviour
+{
+
+    float y;
+    float x;
+    float z;
+    public AudioClip audio;
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+    public int rdn;
+   private bool flag;
+    void Start()
+    {
+
+       
+    }
+
+
+   
+    void Update()
+
+        
+    {
+
+    if((int)Time.time == rdn)
+        {
+            flag = true;
+        }
+
+
+        if (flag)
+        {
+
+            PictureMovement();
+        }
+
+    }
+
+
+   
+
+    public void PictureMovement()
+    {
+       
+        y += -1f * ((int)Time.time-rdn);
+        x += 2 * ((int)Time.time - rdn);
+        
+        if (y > -5)
+        {
+
+            transform.localPosition = new Vector3(0, y, 0);
+            transform.localRotation = Quaternion.Euler(x, 0, 0);
+
+
+            if ((y - 1) == -5)
+            {
+                gameObject.AddComponent<AudioSource>();
+                source.clip = audio;
+                source.playOnAwake = false;
+                source.PlayOneShot(audio);
+            }
+
+        }
+    }
+
+
+   
+}
