556e8b8628cf3e866cb27f141c09c5af0d9a38f8
giomm2 <giomm269@gmail.com>
2016-06-25 17:12:02 -0600

    Examen Giovanny, US: 17,20

---
+++ Assets/_MoveScripts/DoorMovement.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorMovement : MonoBehaviour
+{
+    public float posX;
+    public float posY;
+    public float posZ;
+    public float traY;
+    public GameObject objects;
+    public string nameTrigger;
+    public AudioClip audioOpen;
+    public AudioClip audioClose;
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+    public int timer;
+
+    private int flag = 3;
+    private bool flag2 = true;
+    private bool flag3 = true;
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.name.Equals(nameTrigger))
+        {
+
+            if ((int)Time.time > timer)
+            {
+                flag = 1;
+            }
+
+            if (flag == 1)
+            {
+
+
+
+
+                if (flag2)
+                {
+
+                    objects.transform.localPosition = new Vector3(posX, posY, posZ);
+                    objects.transform.localRotation = Quaternion.Euler(0, traY, 0);
+                    gameObject.AddComponent<AudioSource>();
+                    source.clip = audioOpen;
+                    source.playOnAwake = false;
+                    source.PlayOneShot(audioOpen);
+
+                    flag2 = false;
+
+                }
+
+
+            }
+
+        }
+
+    }
+
+
+    void OnTriggerExit(Collider collider)
+    {
+
+
+        if ((int)Time.time > timer && flag3 == true)
+        {
+            if (collider.name.Equals(nameTrigger))
+            {
+
+
+                objects.transform.localPosition = new Vector3(0, 0, 0);
+                objects.transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+                gameObject.AddComponent<AudioSource>();
+                source.clip = audioClose;
+                source.playOnAwake = false;
+                source.PlayOneShot(audioClose);
+                flag = 3;
+
+                flag3 = false;
+
+            }
+        }
+
+
+    }
+}
---
+++ Assets/_MoveScripts/EnemyProximity.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class EnemyProximity : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject objects;
+    public string nameTrigger;
+    public AudioClip audio;
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+    public int timer;
+    private bool flagEnemy = true;
+    private int enemyEntry;
+    void Awake()
+    {
+        objects.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+    }
+
+
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+
+        if (collider.name.Equals(nameTrigger))
+        {
+
+            if ((int)Time.time >= timer && flagEnemy == true)
+            {
+                 enemyEntry = (int)Time.time+7;
+                objects.SetActive(true);
+                gameObject.AddComponent<AudioSource>();
+                source.clip = audio;
+                source.playOnAwake = false;
+                source.PlayOneShot(audio);
+                flagEnemy = false;
+
+            }
+
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+
+        if (collider.name.Equals(nameTrigger))
+        {
+
+            if ((int)Time.time ==enemyEntry && flagEnemy == false)
+            {
+                objects.SetActive(false);
+
+
+
+
+            }
+        }
+
+    }
+
+}
+
+
